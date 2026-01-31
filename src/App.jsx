08be700edf336350d74ca752c0748d6d0901db17
Placidpio282 <placidpio2024@gmail.com>
2026-01-31 20:47:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import React, { createContext, useEffect } from 'react'
+import Home from './Pages/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
+import { ToastContainer } from 'react-toastify'
+import { useState } from 'react'
+import axios from 'axios'
+import Dashboard from './Pages/Dashboard'
+import Contacts from './Components/Contacts'
+import AddContact from './Components/AddContact'
+import EditContact from './Components/EditContact'
+import Logout from './Components/Logout'
+import ProtectedRoutes from './Components/ProtectedRoutes'
+import NotFound from './Pages/NotFound'
+
+
+
+export const UserContext = createContext(null);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/dashboard',
+    element: <ProtectedRoutes><Dashboard /></ProtectedRoutes>,
+    children: [
+      {
+        index: true,
+        element: <Contacts />
+      },
+      {
+        path: "/dashboard/add-contact",
+        element: <AddContact />
+      }
+      ,
+      {
+        path: "/dashboard/edit-contact/:id",
+        element: <EditContact />
+      }
+    ]
+  },
+  {
+    path: "/logout",
+    element: <Logout />,
+  },
+  {
+    path:"*",
+    element: <NotFound />
+  }
+
+])
+
+const App = () => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3000/contactmsyt/verify', {
+      headers: {
+        Authorization: `Berear ${localStorage.getItem('token')}`//Berear
+      }
+    })
+      .then(res => {
+        if (res.data.success) {
+          setUser(res.data.user)
+
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+
+  }, [])
+  return (
+    <>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <RouterProvider router={router} />
+      </UserContext.Provider >
+    </>
+  );
+};
+
+
+export default App;
